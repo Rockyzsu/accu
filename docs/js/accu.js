e401896b011fcf00c55ad30e023c44d53ab39d30
mohanson <mohanson@outlook.com>
2022-09-14 18:01:28 +0800

    2022-09-14 18:01:28

--- docs/js/accu.js
+++ docs/js/accu.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    SphinxRtdTheme.Navigation.onScroll = function () { }
+
     if ($('li.current').length) {
         $('.wy-side-scroll').scrollTop(
             $('li.current').offset().top - $('.wy-side-scroll').offset().top - 120
